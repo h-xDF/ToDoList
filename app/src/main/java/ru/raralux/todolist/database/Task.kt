39e15696b8681f848de98f4e5567233c33dc1289
Yaroslav Pereyaslov <y.pereyaslov@semrush.com>
2020-10-04 21:49:42 +0300

    fix type taskId

--- app/src/main/java/ru/raralux/todolist/database/Task.kt
+++ app/src/main/java/ru/raralux/todolist/database/Task.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Task constructor(@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "task_id") val taskId: Long,
+data class Task constructor(@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "task_id") val taskId: Int,
                             @ColumnInfo(name = "name") val name: String,
                             @ColumnInfo(name = "description") val description: String,
                             @ColumnInfo(name = "visible") val visible: Boolean)
